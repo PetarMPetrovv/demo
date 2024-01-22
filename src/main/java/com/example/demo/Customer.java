304641d10c6326362313a89188aa98156e241c60
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-01-22 23:21:03 +0200

    added getters and setters in Customer class and added a customer repo

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -15,4 +15,37 @@ public class Customer {
     private String phoneNumber;
     private String linkToPropertyAd;
     private List<String> callNotes;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLinkToPropertyAd() {
+        return linkToPropertyAd;
+    }
+
+    public void setLinkToPropertyAd(String linkToPropertyAd) {
+        this.linkToPropertyAd = linkToPropertyAd;
+    }
+
+    public List<String> getCallNotes() {
+        return callNotes;
+    }
+
+    public void setCallNotes(List<String> callNotes) {
+        this.callNotes = callNotes;
+    }
 }
