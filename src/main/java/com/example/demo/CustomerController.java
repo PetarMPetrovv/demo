9fa426bb0cbe4da219f4902f4bbca2002fab5180
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-01-24 19:00:39 +0200

    work on customer controller but still need much work

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -9,5 +9,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class CustomerController {
     private CustomerRepository customerRepository;
-
+    @Autowired
+    public ReadingListController(
+            ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+    @RequestMapping(value="/{reader}", method=RequestMethod.GET)
+    public String readersBooks(
+            @PathVariable("reader") String reader,
+            Model model) {
+        List<Book> readingList =
+                readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }
+    Listing 2.6 A Spring MVC controller that fronts the reading list application
+    Licensed to Thomas Snead <n.ordickan@gmail.com>
+    Using automatic configuration 41
+    @RequestMapping(value="/{reader}", method=RequestMethod.POST)
+    public String addToReadingList(
+            @PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/{reader}";
+    }
 }
