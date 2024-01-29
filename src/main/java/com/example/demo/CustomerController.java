cfae2ba0b9dfbfe3d54cdd47cf826afaaf018fc8
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-01-29 10:01:53 +0200

    deleted some parts from the controller but its runablle now

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -1,10 +1,8 @@
 package com.example.demo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -12,21 +10,11 @@ import java.util.List;
 @RequestMapping("/")
 public class CustomerController {
     private CustomerRepository customerRepository;
-    @Autowired
-    public CustomerController(
-            CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-    @RequestMapping(value="/{customer}", method=RequestMethod.GET)
-    public String customers(
-            @PathVariable("customer") String customer,
-            Model model) {
-        List<Customer> customers =
-                customerRepository.findByReader(reader);
-        if (readingList != null) {
-            model.addAttribute("books", readingList);
-        }
-        return "readingList";
-    }
 
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Customer> customers = customerRepository.findAll();
+        model.addAttribute("customers", customers);
+        return "home";
+    }
 }
