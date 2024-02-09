3839afe1c63732e8534e0fe5c9069bf28be855c9
PetarMPetrov <petrov.miroslavov@gmail.com>
2024-02-09 08:25:24 +0200

    changed generated type of id to auto

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -8,7 +8,7 @@ import java.util.List;
 @DiscriminatorColumn(name = "customer_type", discriminatorType = DiscriminatorType.STRING)
 public class Customer {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String name;
     private String phoneNumber;
